92873381a0c4e5082f46c3893edb46e5d732fea6
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-21 17:14:40 -0300

    NEW - Estilização da exibição do repositório

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Loading, Owner } from './styles';
+import Container from '../../components/Container';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -45,6 +47,19 @@ export default class Repository extends Component {
   render() {
     const { loading, repository, issues } = this.state;
 
-    return <h1>Repository: </h1>;
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    );
   }
 }
