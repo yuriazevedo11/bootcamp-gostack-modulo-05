c1ad4b66490e90c7c47c3a68490bf1c003ea3dc5
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-21 16:52:52 -0300

    NEW - Carregando dados da API do Github

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  state = {
+    loading: true,
+    repository: {},
+    issues: [],
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`repos/${repoName}`),
+      api.get(`repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      loading: false,
+      repository: repository.data,
+      issues: issues.data,
+    });
+  }
+
+  render() {
+    const { loading, repository, issues } = this.state;
+
+    return <h1>Repository: </h1>;
+  }
 }
